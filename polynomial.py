98bf83fe20fe66f70bfbaff8a4750a702d67216a
Luca Baroni <lucabaronih@gmail.com>
2018-01-08 18:42:39 +0100

    Update polynomial.py: remove reference to Python 3

--- polynomial.py
+++ polynomial.py
@@ -1,5 +1,3 @@
-# Python 3
-
 class Polynomial:
     def __init__(self, terms):
         if type(terms) != list or any(type(t) != tuple or len(t) != 2 for t in terms):
@@ -12,10 +10,10 @@ class Polynomial:
         for (c1, exp1), (c2, exp2) in zip(self.terms, self.terms[1:]):
             if exp1 == exp2:
                 raise Exception('duplicate term of degree %s' % exp1)
-    
+
     def degree(self):
         return -1 if len(self.terms) == 0 else self.terms[-1][1]
-    
+
     def evaluate(self, k):
         '''Substitute k to the indeterminate x and return the value of the
         resulting expression.
@@ -23,10 +21,10 @@ class Polynomial:
         if type(k) not in (int, float):
             raise TypeError('can only substitute an integer or a float to the indeterminate x')
         return sum((c*k**exp for c, exp in self.terms))
-    
+
     def __repr__(self):
         return 'Polynomial({terms})'.format(terms = self.terms)
-    
+
     def __str__(self):
         if self.degree() == -1:
             return '0'
@@ -40,11 +38,11 @@ class Polynomial:
                     s += '**' + str(exp) if exp > 1 else ''
             # the leading sign is included only if it is a minus
             return s[1:] if s[1] == '-' else s[3:]
-    
+
     def __bool__(self):
         # return False if the Polynomial object is the zero polynomial, else True.
         return self.degree() != -1
-    
+
     def __eq__(self, other):
         if type(other) == Polynomial:
             return self.terms == other.terms
@@ -52,7 +50,7 @@ class Polynomial:
             return self.terms == [(other, 0)] * int(other != 0)
         else:
             return False
-    
+
     def __add__(self, other):
         if type(other) not in (int, float, Polynomial):
             raise TypeError('%s object cannot be interpreted as a Polynomial' % type(other).__name__)
@@ -74,29 +72,29 @@ class Polynomial:
             return Polynomial(result + other_polynomial.terms[j:])
         else:
             return Polynomial(result + self.terms[i:])
-    
+
     def __radd__(self, other):
         return self + other
-    
+
     def __sub__(self, other):
         if type(other) not in (int, float, Polynomial):
             raise TypeError('%s object cannot be interpreted as a Polynomial' % type(other).__name__)
         other_polynomial = other if type(other) == Polynomial else Polynomial([(other, 0)])
         return -1*other + self
-    
+
     def __rsub__(self, other):
         return -1*self + other
-    
+
     def __mul__(self, other):
         if type(other) not in (int, float, Polynomial):
             raise TypeError('%s object cannot be interpreted as a Polynomial' % type(other).__name__)
         other_polynomial = other if type(other) == Polynomial else Polynomial([(other, 0)])
         return sum((Polynomial([(c1 * c2, exp1 + exp2) for c1, exp1 in self.terms])
                    for c2, exp2 in other_polynomial.terms), Polynomial([]))
-    
+
     def __rmul__(self, other):
         return self * other
-    
+
     def __truediv__(self, other):
         from collections import namedtuple
         if type(other) not in (int, float, Polynomial):
@@ -120,7 +118,7 @@ class Polynomial:
         # the quotient and the remainder
         Polynomial_division = namedtuple('Polynomial_division', ['quotient', 'remainder'])
         return Polynomial_division(quotient = quotient, remainder = remainder)
-    
+
     def __pow__(self, n):
         import functools
         if type(n) != int:
@@ -128,7 +126,7 @@ class Polynomial:
         if n < 0:
             raise ValueError('the exponent must be greater than or equal to zero')
         return functools.reduce(lambda x,y: x*y , (Polynomial(self.terms) for i in range(n)), 1)
-    
+
     def derivative(self, n=1):
         '''Return the nth derivative of the polynomial.'''
         import functools
@@ -140,7 +138,7 @@ class Polynomial:
         for c, exp in self.terms:
             result.append((functools.reduce(lambda x,y: x*y, range(exp-n+1,exp+1), c), max(exp-n, 0)))
         return Polynomial(result)
-    
+
     def integral(self, c=0):
         '''Return the primitive integral of the polynomial. The constant factor is equal to c.'''
         if type(c) not in (int, float):
