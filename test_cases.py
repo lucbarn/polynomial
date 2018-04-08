d4c8ceaba1445475ad59c4937d83ec034e5f8218
Luca <lucabaronih@gmail.com>
2018-04-08 21:36:34 +0200

    Rewrite test_cases.py using unittest

--- test_cases.py
+++ test_cases.py
@@ -1,45 +1,92 @@
+import unittest
 from polynomial import Polynomial
 from utils import pascals_triangle, binomial_expansion
 from random import randint
 
-def test():
 
-    x = Polynomial([(1,1)])
+class TestPolynomialMethods(unittest.TestCase):
 
-    assert x**0 == 1
-    assert x**1 == 0 + x == x + 0 == x
-    assert x - x == x * 0 == 0
-    assert x + x == 2 * x
-    assert (x+1)**2 == x**2 + 2 * x + 1
-    assert (x-1)**3 == x**3 - 3*x**2 + 3*x - 1
-    assert (x**2 - 1) == (x - 1) * (x + 1)
-    assert (x-4)**100 == binomial_expansion(x,-4,100)
-    assert x.derivative().integral() == x.integral().derivative() == x
+    def setUp(self):
+        self.x = Polynomial([(1,1)])
 
-    # test cases for polynomial long division
+    def test_zero_power(self):
+        self.assertEqual(self.x**0, 1, 'x**0 should be equal to 1')
 
-    den1 = x**2 + 2*x + 3
-    q1 = 2*x
-    r1 = x + 5
-    num1 = den1 * q1 + r1
-    div1 = num1 / den1
-    assert num1 == den1 * div1.quotient + div1.remainder
+    def test_one_power(self):
+        self.assertEqual(self.x**1, self.x, 'x**1 should be equal to x')
 
-    den1 = 9 * x**4 + 6 * x**3 + 2 * x**2 + 4 * x
-    q1 = 2 * x**2 + 2 * x + 5
-    r1 = x**2 + 4
-    num1 = den1 * q1 + r1
-    div1 = num1 / den1
-    assert num1 == den1 * div1.quotient + div1.remainder
+    def test_zero_sum(self):
+        self.assertEqual(self.x + 0, self.x, 'x+0 should be equal to x')
 
-    # test cases for polynomial degree
+    def test_self_difference(self):
+        self.assertEqual(self.x - self.x, 0, 'x-x should be equal to 0')
 
-    p = Polynomial([])
-    for n in range(100):
-        p += randint(1,10) * x**n
-        assert p.degree() == n
+    def test_sum_product_equivalence(self):
+        self.assertEqual(self.x + self.x, self.x * 2, 'x+x should be equal to 2x')
+
+    def test_derivative_integral_concatenation(self):
+        self.assertEqual(self.x.derivative().integral(),
+                         self.x,
+                         'the integral of the derivative of x should be equal to x')
+
+
+class TestPolynomialExpansion(unittest.TestCase):
+
+    def setUp(self):
+        self.x = Polynomial([(1,1)])
+
+    def test_square_binomial(self):
+        self.assertEqual((self.x + 1) ** 2, self.x**2 + 2 * self.x + 1, '(x+1)**2 should be equal to x**2 + 2x + 1')
+
+    def test_cube_binomial(self):
+        self.assertEqual((self.x - 1) ** 3,
+                    self.x**3 - 3 * self.x**2 + 3 * self.x - 1,
+                    '(x-1)**3 should be equal to x**3 - 3x**2 + 3x - 1')
+
+    def test_difference_squares(self):
+        self.assertEqual((self.x**2 - 1),
+                    (self.x + 1) * (self.x - 1),
+                    'x**2 - 1 should be equal to (x+1) * (x-1)')
+
+    def test_big_exponent(self):
+        self.assertEqual((self.x - 4) ** 100,
+                    binomial_expansion(self.x, -4, 100),
+                    '(x-4)**100 should be equal to the binomial expansion of (x-4) computed using Pascal\'s triangle')
+
+
+class TestPolynomialLongDivision(unittest.TestCase):
+
+    def setUp(self):
+        self.x = Polynomial([(1,1)])
+
+    def test_division_1(self):
+        den = self.x**2 + 2 * self.x + 3
+        q = 2 * self.x
+        r = self.x + 5
+        num = den * q + r
+        div = num / den
+        self.assertEqual(num, den * div.quotient + div.remainder)
+
+    def test_division_2(self):
+        den = 9 * self.x**4 + 6 * self.x**3 + 2 * self.x**2 + 4 * self.x
+        q = 2 * self.x**2 + 2 * self.x + 5
+        r = self.x**2 + 4
+        num = den * q + r
+        div = num / den
+        self.assertEqual(num, den * div.quotient + div.remainder)
+
+
+class TestPolynomialDegree(unittest.TestCase):
+
+    def setUp(self):
+        self.p = Polynomial([])
+        self.x = Polynomial([(1,1)])
+
+    def test_polynomial_degree(self):
+        for n in range(100):
+            self.p += randint(1,10) * self.x**n
+            self.assertEqual(self.p.degree(), n)
 
 
 if __name__ == '__main__':
-    test()
-    print('Test passed!')
+    unittest.main()
