78224e9c1eaabe0ea41749ad759191e09e4e53e5
Luca <lucabaronih@gmail.com>
2018-05-23 21:27:42 +0200

    Add exceptions

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 def pascals_triangle(m):
     '''Returns the mth row (starting from 0) of the Pascal's triangle'''
+    if type(m) != int:
+        raise Exception('m must be an integer')
+    if m < 0:
+        raise Exception('m cannot be negative')
     current_row = [1]
     for i in range(1,m+1):
         new_row = []
@@ -10,6 +14,10 @@ def pascals_triangle(m):
 
 def binomial_expansion(a,b,n):
     '''Returns the result of (a+b)**n'''
+    if type(n) != int:
+        raise Exception('n must be an integer')
+    if n < 0:
+        raise Exception('n cannot be negative')
     coefficients = pascals_triangle(n)
     output = 0
     for k in range(n+1):
