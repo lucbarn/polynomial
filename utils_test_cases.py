bef447f85ff90e72acb3d1cffd87662e4cdc3281
Luca <lucabaronih@gmail.com>
2018-05-23 21:49:10 +0200

    Add test cases for utils.py

---
+++ utils_test_cases.py
@@ -0,0 +1,24 @@
+import unittest
+from utils import pascals_triangle, binomial_expansion
+
+class TestPascalsTriangle(unittest.TestCase):
+
+    def test_first_row(self):
+        self.assertEqual(pascals_triangle(0), [1], 'first row should contain only 1')
+
+    def test_second_row(self):
+        self.assertEqual(pascals_triangle(1), [1,1], 'second row should contain two elements equal to 1')
+
+    def test_eigth_row(self):
+        self.assertEqual(pascals_triangle(7), [1,7,21,35,35,21,7,1], 'eigth row should contain 1,7,21,35,35,21,7,1')
+
+class TestBinomialExpansion(unittest.TestCase):
+
+    def test_power_zero(self):
+        self.assertEqual(binomial_expansion(1,2,0), 1, 'a binomial raised to the power of zero should be equal to 1')
+
+    def test_power_one(self):
+        self.assertEqual(binomial_expansion(-2,4,1), -2+4, 'a binomial raised to the power of 1 should be equal to itself')
+
+if __name__ == '__main__':
+    unittest.main()
